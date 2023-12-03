86a4e3b80338df935f1d7c0f626606c17306a310
Your Name <you@example.com>
2023-12-03 12:13:35 +0545

    first change

---
+++ app/src/main/java/com/thecodecity/pdfcreator/MainActivity.java
@@ -0,0 +1,154 @@
+package com.thecodecity.pdfcreator;
+
+import android.Manifest;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+    final static int REQUEST_CODE = 1232;
+    Button btnCreatePDF;
+    Button btnXMLtoPDF;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        askPermissions();
+        btnCreatePDF = findViewById(R.id.btnCreatePdf);
+        btnCreatePDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPDF();
+            }
+        });
+
+        btnXMLtoPDF = findViewById(R.id.btnXMLToPDF);
+        btnXMLtoPDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertXmlToPdf();
+            }
+        });
+
+    }
+
+    private void askPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+    }
+
+    private void createPDF() {
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1080, 1920, 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setTextSize(42);
+
+        String text = "Hello, World";
+        float x = 500;
+        float y = 900;
+
+        canvas.drawText(text, x, y, paint);
+        document.finishPage(page);
+
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String fileName = "example.pdf";
+        File file = new File(downloadsDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            Toast.makeText(this, "Written Successfully!!!", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            Log.d("mylog", "Error while writing " + e.toString());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void convertXmlToPdf() {
+        // Inflate the XML layout file
+        View view = LayoutInflater.from(this).inflate(R.layout.main_activity, null);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            this.getDisplay().getRealMetrics(displayMetrics);
+        } else
+            this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        view.measure(View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(displayMetrics.heightPixels, View.MeasureSpec.EXACTLY));
+        Log.d("mylog", "Width Now " + view.getMeasuredWidth());
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        // Create a new PdfDocument instance
+        PdfDocument document = new PdfDocument();
+
+        // Obtain the width and height of the view
+        int viewWidth = view.getMeasuredWidth();
+        int viewHeight = view.getMeasuredHeight();
+        // Create a PageInfo object specifying the page attributes
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(viewWidth, viewHeight, 1).create();
+
+        // Start a new page
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        // Get the Canvas object to draw on the page
+        Canvas canvas = page.getCanvas();
+
+        // Create a Paint object for styling the view
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+
+        // Draw the view on the canvas
+        view.draw(canvas);
+
+        // Finish the page
+        document.finishPage(page);
+
+        // Specify the path and filename of the output PDF file
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String fileName = "exampleXML.pdf";
+        File filePath = new File(downloadsDir, fileName);
+
+        try {
+            // Save the document to a file
+            FileOutputStream fos = new FileOutputStream(filePath);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            // PDF conversion successful
+            Toast.makeText(this, "XML to PDF Conversion Successful", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Error occurred while converting to PDF
+        }
+    }
+
+
+}
